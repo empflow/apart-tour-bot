f9241a4e2c56f717873f66c25de75b3924e1e553
Matvey <empflow@tutanota.com>
2023-07-27 19:58:55 +0300

    showMainMenu -> mainMenu

--- src/commands/mainMenu.ts
+++ src/commands/mainMenu.ts
@@ -2,7 +2,7 @@ import bot from "../bot";
 import responseTexts from "../responseTexts";
 import getMainMenuButtons from "../utils/getMainMenuButtons";
 
-export default function showMainMenu(chatId: number) {
+export default function mainMenu(chatId: number) {
   const inline_keyboard = getMainMenuButtons();
   bot.sendMessage(chatId, responseTexts.mainMenu, {
     reply_markup: { inline_keyboard },

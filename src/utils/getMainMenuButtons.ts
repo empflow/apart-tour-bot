3513dadfbec00321e86b403d8f86267c7389f0af
Matvey <empflow@tutanota.com>
2023-07-27 19:10:06 +0300

    Create getMainMenuButtons.ts

---
+++ src/utils/getMainMenuButtons.ts
@@ -0,0 +1,14 @@
+import {
+  InlineKeyboardButton,
+  InlineKeyboardMarkup,
+} from "node-telegram-bot-api";
+import {
+  buyTourButton,
+  examplesButton,
+  learnMoreButton,
+  whyButton,
+} from "./buttons";
+
+export default function getMainMenuButtons(): InlineKeyboardButton[][] {
+  return [[learnMoreButton], [whyButton], [examplesButton], [buyTourButton]];
+}

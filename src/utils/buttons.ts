4471c1ec96082f864335e4ebd9707b24a3e70339
Matvey <empflow@tutanota.com>
2023-07-27 19:10:47 +0300

    Create buttons.ts

---
+++ src/utils/buttons.ts
@@ -0,0 +1,19 @@
+export const learnMoreButton = {
+  text: "📖 Подробнее про stop-motion туры",
+  callback_data: "learnMore",
+};
+
+export const whyButton = {
+  text: "❓ Зачем мне нужен stop-motion тур?",
+  callback_data: "why",
+};
+
+export const examplesButton = {
+  text: "👀 Посмотреть примеры работ",
+  callback_data: "examples",
+};
+
+export const buyTourButton = {
+  text: "🎥 Хочу отснять недвижимость",
+  callback_data: "buyTour",
+};

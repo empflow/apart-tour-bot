e75200b53e65458314dc9d27050bfaa6e4ce9722
Matvey <empflow@tutanota.com>
2023-07-28 17:18:14 +0300

    log polling errors in db

--- src/listeners/pollingError.ts
+++ src/listeners/pollingError.ts
@@ -1,5 +1,11 @@
 import bot from "../bot";
+import ErrorModel from "../models/ErrorModel";
 
-bot.on("polling_error", (err) => {
-  console.error(err);
+bot.on("polling_error", async (err) => {
+  err = err as any;
+  await ErrorModel.create({
+    type: "polling",
+    message: err.message,
+    stacktrace: err.stack,
+  });
 });

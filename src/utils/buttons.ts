319d4a7113d6ccee72af59e85bd1617e973f3e12
Matvey <empflow@tutanota.com>
2023-07-28 10:19:53 +0300

    add messageDirectlyButton & add types for all buttons

--- src/utils/buttons.ts
+++ src/utils/buttons.ts
@@ -1,19 +1,26 @@
-export const learnMoreButton = {
+import { InlineKeyboardButton } from "node-telegram-bot-api";
+
+export const learnMoreButton: InlineKeyboardButton = {
   text: "📖 Подробнее про stop-motion туры",
   callback_data: "learnMore",
 };
 
-export const whyButton = {
+export const whyButton: InlineKeyboardButton = {
   text: "❓ Зачем мне нужен stop-motion тур?",
   callback_data: "why",
 };
 
-export const examplesButton = {
+export const examplesButton: InlineKeyboardButton = {
   text: "👀 Посмотреть примеры работ",
   callback_data: "examples",
 };
 
-export const buyTourButton = {
+export const buyTourButton: InlineKeyboardButton = {
   text: "🎥 Хочу отснять недвижимость",
   callback_data: "buyTour",
+};
+
+export const messageDirectlyButton: InlineKeyboardButton = {
+  text: "📞 Связаться со мной",
+  callback_data: "messageDirectly",
 };

798433320ffd34c2efe03f52468a7440939b6e3a
Matvey <empflow@tutanota.com>
2023-07-28 10:42:07 +0300

    Update messageDirectly.ts

--- src/commands/messageDirectly.ts
+++ src/commands/messageDirectly.ts
@@ -1,16 +1,30 @@
-import { KeyboardButton } from "node-telegram-bot-api";
+import {
+  InlineKeyboardMarkup,
+  ReplyKeyboardMarkup,
+} from "node-telegram-bot-api";
 import bot from "../bot";
 import responseTexts from "../responseTexts";
+import { backToMenuButton } from "../utils/buttons";
 
 export default function messageDirectly(chatId: number) {
-  const keyboard: KeyboardButton[][] = [
-    [{ text: "Отправить мой номер телефона", request_contact: true }],
-  ];
+  const replyMarkupWithKeyboard: ReplyKeyboardMarkup = {
+    keyboard: [
+      [{ text: "Отправить мой номер телефона", request_contact: true }],
+    ],
+    resize_keyboard: true,
+  };
+
+  const replyMarkupWithInlineKeyboard: InlineKeyboardMarkup = {
+    inline_keyboard: [[backToMenuButton]],
+  };
 
   // is_persistent always keeps the button keyboard open when the regular keyboard is hidden
   // one_time_keyboard hides the buttons keyboard as soon as one of the buttons has been pressed (can be re-opened with a keyboard icon)
 
   bot.sendMessage(chatId, responseTexts.messageDirectly, {
-    reply_markup: { keyboard, resize_keyboard: true },
+    reply_markup: replyMarkupWithKeyboard,
+  });
+  bot.sendMessage(chatId, "Или можете вернуться в меню:", {
+    reply_markup: replyMarkupWithInlineKeyboard,
   });
 }

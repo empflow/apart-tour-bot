394bd9a5e746159ab75f60b015ab6087f882a69e
Matvey <empflow@tutanota.com>
2023-07-28 12:44:11 +0300

    show sendMyPhoneNumberKbBtn on seePrices & remove backToMenuBtn

--- src/commands/seePrices.ts
+++ src/commands/seePrices.ts
@@ -1,10 +1,13 @@
 import bot from "../bot";
 import responseTexts from "../responseTexts";
-import { backToMenuCbBtn } from "../utils/buttons";
+import { backToMenuCbBtn, sendMyPhoneNumberKbBtn } from "../utils/buttons";
 
 export default function seePrices(chatId: number) {
   bot.sendMessage(chatId, responseTexts.seePrices, {
-    reply_markup: { inline_keyboard: [[backToMenuCbBtn]] },
     parse_mode: "HTML",
+    reply_markup: {
+      keyboard: [[sendMyPhoneNumberKbBtn]],
+      one_time_keyboard: true,
+    },
   });
 }

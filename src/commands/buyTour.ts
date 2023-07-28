7668117698fc8bbf339c939244297cb0ca088634
Matvey <empflow@tutanota.com>
2023-07-28 11:53:16 +0300

    use seePricesButton in buyTour.ts

--- src/commands/buyTour.ts
+++ src/commands/buyTour.ts
@@ -1,14 +1,10 @@
-import { InlineKeyboardButton } from "node-telegram-bot-api";
 import bot from "../bot";
 import responseTexts from "../responseTexts";
-import getMainMenuButtonsExcluding from "../utils/getMainMenuButtonsExcluding";
+import { seePricesButton } from "../utils/buttons";
 
 export default function buyTour(chatId: number) {
-  const inline_keyboard: InlineKeyboardButton[][] = [
-    [{ text: "Узнать стоимость", callback_data: "seePrices" }],
-  ];
-
   bot.sendMessage(chatId, responseTexts.buyTour, {
-    reply_markup: { inline_keyboard },
+    reply_markup: { inline_keyboard: [[seePricesButton]] },
+    parse_mode: "HTML",
   });
 }

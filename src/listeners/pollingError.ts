e8736ca2bbe6e149bd897aa13325b672fc56eac5
Matvey <empflow@tutanota.com>
2023-07-28 17:45:51 +0300

    log polling errors to console

--- src/listeners/pollingError.ts
+++ src/listeners/pollingError.ts
@@ -3,6 +3,9 @@ import ErrorModel from "../models/ErrorModel";
 
 bot.on("polling_error", async (err) => {
   err = err as any;
+  console.error("POLLING ERROR");
+  console.error(err.message);
+
   await ErrorModel.create({
     type: "polling",
     message: err.message,

da2f5fceff272be9860d487e3e370d091fada8bf
Matvey <empflow@tutanota.com>
2023-07-29 12:28:00 +0300

    forward contact info to a private group chat

--- src/listeners/contact.ts
+++ src/listeners/contact.ts
@@ -1,14 +1,18 @@
 import { Contact as IContact } from "node-telegram-bot-api";
 import bot from "../bot";
+import { ownerWithBotGroupChatId } from "../config";
 import Contact from "../models/Contact";
 import respTexts from "../respTexts";
-import { backToMenuCbBtn } from "../utils/buttons";
 import isErrDuplicateErr from "../utils/isErrDuplicateErr";
 import sendErrMsg from "../utils/sendErrMsg";
 import { backToMenuBtnOpts } from "../utils/sendMsgOpts";
 
 bot.on("contact", async (msg) => {
   const chatId = msg.chat.id;
+
+  if (isNaN(ownerWithBotGroupChatId)) {
+    return sendErrMsg(chatId, respTexts.err, backToMenuBtnOpts);
+  }
   if (!msg.contact) return sendErrMsg(chatId);
 
   try {
@@ -24,10 +28,20 @@ bot.on("contact", async (msg) => {
     return sendErrMsg(chatId, respTexts.savingPhoneNumErr, backToMenuBtnOpts);
   }
 
-  bot.sendMessage(chatId, respTexts.contactReceived, {
-    reply_markup: { inline_keyboard: [[backToMenuCbBtn]] },
-  });
+  const formattedContact = formatContact(msg.contact);
+  bot.sendMessage(ownerWithBotGroupChatId, formattedContact);
+  bot.sendMessage(chatId, respTexts.contactReceived, backToMenuBtnOpts);
 });
+
+function formatContact(contact: IContact) {
+  let result = `First name: ${contact.first_name}`;
+  if (contact.last_name) result += `\nLast name: ${contact.last_name}`;
+  result += `\nPhone number: ${contact.phone_number}`;
+  if (contact.user_id) result += `\nUser id: ${contact.user_id}`;
+  if (contact.vcard) result += `\nVcard: ${contact.vcard}`;
+
+  return result;
+}
 
 async function saveContactToDb(contact: IContact) {
   const { phone_number, first_name, last_name, user_id, vcard } = contact;

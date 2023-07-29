ebe5b680c4a3c9a778e10c1e188c62d0e43db1cc
Matvey <empflow@tutanota.com>
2023-07-29 10:52:02 +0300

    allow sending images using string file paths

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,7 @@ import TelegramBot from "node-telegram-bot-api";
 import getEnvVar from "./utils/getEnvVar";
 
 const token = getEnvVar("TOKEN");
-const bot = new TelegramBot(token, { polling: true, filepath: false });
+const bot = new TelegramBot(token, { polling: true });
 console.log("Bot started");
 
 export default bot;
